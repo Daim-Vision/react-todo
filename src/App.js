1a6c426908edad8ad7f923a667b4e141307f902a
Daim <muslim_abdulov@mail.ru>
2023-02-16 22:58:24 +0300

    Выполнено

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Form from "./Form";
+import Header from "./Header";
+import "./styles.css";
+import Todos from "./Todos";
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Купить бананы",
+      favorite: false,
+    },
+
+    {
+      text: "Купить дом",
+      favorite: true,
+    },
+
+    {
+      text: "Выучить JS",
+      favorite: false,
+    },
+  ]);
+
+  const [text, setText] = useState("");
+
+  const deleteTodo = (indexOfItem) => {
+    const filtered = todos.filter((todo, index) => {
+      if (index === indexOfItem) {
+        return false;
+      }
+      return true;
+    });
+    setTodos(filtered);
+  };
+
+  const makeFavorite = (i) => {
+    const newTodos = todos.map((item, index) => {
+      if (i === index) {
+        return {
+          ...item,
+          favorite: !item.favorite,
+        };
+      }
+      return item;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo = () => {
+    if (text) {
+      setTodos([
+        ...todos,
+        {
+          text: text,
+          favorite: false,
+        },
+      ]);
+      setText("");
+    } else {
+      alert("Поле не должно пустым.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Form text={text} setText={setText} addTodo={addTodo} />
+      <Todos
+        todos={todos}
+        makeFavorite={makeFavorite}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
